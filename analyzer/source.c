556e649ba1d9948f174a23569eba875f021aa5d4
Gonzalo José Carracedo Carballal <BatchDrake@gmail.com>
2022-01-24 10:33:55 +0100

    Fix Win32 absurdities

--- analyzer/source.c
+++ analyzer/source.c
@@ -39,6 +39,9 @@
 #ifdef _WIN32
 #  include <winsock2.h>
 #  include <ws2tcpip.h>
+#  if defined(interface)
+#    undef interface
+#  endif /* interface */
 #endif /* _WIN32 */
 
 #ifdef _SU_SINGLE_PRECISION
