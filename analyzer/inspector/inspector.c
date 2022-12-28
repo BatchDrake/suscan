237196b6e74b59ae9af25927721f907c3c255c17
Gonzalo J. Carracedo <BatchDrake@gmail.com>
2022-12-28 12:26:54 +0100

    Add multicarrier inspector

--- analyzer/inspector/inspector.c
+++ analyzer/inspector/inspector.c
@@ -1051,6 +1051,8 @@ suscan_init_inspectors(void)
   SU_TRYCATCH(suscan_fsk_inspector_register(),   return SU_FALSE);
   SU_TRYCATCH(suscan_audio_inspector_register(), return SU_FALSE);
   SU_TRYCATCH(suscan_raw_inspector_register(),   return SU_FALSE);
+  SU_TRYCATCH(suscan_multicarrier_inspector_register(),   return SU_FALSE);
+
 
   return SU_TRUE;
 }
